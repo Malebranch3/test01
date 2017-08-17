7c705d800ff4f88543870a227f3619a2b03ed329
Gal Doron <galdwd@gmail.com>
2017-08-17 15:00:07 -0500

    Add Methods to Cat
    Added methods to Cat after considering test.

--- index.php
+++ index.php
@@ -4,4 +4,22 @@ class Cat{
 	private $name = '';
 	private $call = 'Meow';
 	private $age = 1;
+	private $spoken = 0;
+	
+	private function setName($newname){
+		$this->name = $newname;
+	}
+	
+	private function speak(){
+		$this->checkSpoken();
+		print($call);
+	}
+
+	private function checkSpoken(){
+		$this->spoken++;
+		if(5 === $this->spoken){
+			$this->ageUp();
+			$this->spoken = 0;
+		}
+	}
 }
