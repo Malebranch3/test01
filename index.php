6c1cdc34a5338dda25db958526c7d0814cdae1f5
Gal Doron <galdwd@gmail.com>
2017-08-17 17:22:02 -0500

    Add Dog class
    Added dog class

--- index.php
+++ index.php
@@ -37,6 +37,43 @@ class Cat{
 	}
 }
 
+class Dog{
+	private $variables = array('name','call'=>'bark','age'=>1,'spoken'=>0);
+	
+	public function getClassVar($var){
+		if(isset($this->variables[$var]) || array_key_exists($this->variables[$var], $this->variables)){
+			return array('success'=>1,'result'=>$this->variables[$var]);
+		}else{
+			return array('success'=>0,'result'=>'Error: No such variable exists.');
+		}
+	}
+	
+	public function setName($newname){
+		$this->variables['name'] = $newname;
+	}
+	
+	public function speak(){
+		$this->checkSpoken();
+		print($this->variables['call']);
+	}
+
+	public function checkSpoken(){
+		$this->incrementSpoken();
+		if(5 === $this->variables['spoken']){
+			$this->ageUp();
+			$this->variables['spoken'] = 0;
+		}
+	}
+	
+	public function incrementSpoken(){
+		$this->variables['spoken']++;
+	}
+	
+	public function ageUp(){
+		$this->variables['age']++;
+	}
+}
+
 class Tests{
 	
 	private $patient = '';
